7ae3e226317d3940c86dac50c4de8697dbca9ff4
Ovidijus <ovidijus.e@gmail.com>
2022-06-29 10:03:21 +0300

    home page

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import { Link, NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to={'/'}>Logo</Link>
+      <nav>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/posts'}>Posts</NavLink>
+        <NavLink to={'/login'}>Login</NavLink>
+        <NavLink to={'/register'}>Register</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
